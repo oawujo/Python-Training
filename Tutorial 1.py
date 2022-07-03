9b7ccf8966e665be45f8a63dd2440f72fef830fe
oawujo@gmail.com <C4rrera4$>
2022-07-03 13:47:33 +0100

    Tutorials

---
+++ Tutorial 1.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Python Exercises
+
+# # Exercise 1: Create a list by picking an odd-index items from the first list and even index items from the second.
+# 
+# Given two lists, l1 and l2, write a program to create a third list l3 by picking an odd-index elements from the list l1 and even index elements from the list l2.
+# 
+# l1 = [3, 6, 9, 12, 15, 18, 21]
+# 
+# l2 = [4, 8, 13, 16, 20, 24, 28]
+
+# # ****HINT***
+# use slicing
+# 
+# # Expected Output
+# 
+# Element at odd-index positions from list one
+# 
+# [6, 12, 18]
+# 
+# Element at even-index positions from list two
+# 
+# [4, 12, 20, 28]
+# 
+# Printing Final third list
+# 
+# [6, 12, 18, 4, 12, 20, 28]
+
+# In[ ]:
+
+
+#your code here
+
+l1 = [2,3,4]
+print(l1)
+# # Exercise 2: Remove and add item in a list
+# 
+# Write a program to remove the item present at index 4 and add it to the 2nd position and at the end of the list.
+# 
+# list1[34, 54, 67, 89, 11, 43, 94]
+
+# # ***HINT***
+# Use the list methods, pop(), insert() and append()
+# 
+# 
+# # Expected Output:
+# 
+# 
+# List After removing element at index 4  [34, 54, 67, 89, 43, 94]
+# 
+# List after Adding element at index 2  [34, 54, 11, 67, 89, 43, 94]
+# 
+# List after Adding element at last  [34, 54, 11, 67, 89, 43, 94, 11]
+
+# In[ ]:
+
+
+#yur code here
+
+
+
+# # Exercise 3
+# 
+# Write a program to finad all occurrences of "NIGERIA" in a given string ignoring the cases.
+# 
+# str1 ="NIGERIA is great. only that  politicians in nigeria have killed nigeria"
+
+# # ****HINT****
+# use sub str.
+# use the string function count( )
+# 
+# # expected output
+# The NIGERIA count is: 3
+# 
+
+# In[ ]:
+
+
+#your code here
+
+
