8044127a26ea344a04befa62998ddf373d046b9e
oawujo@gmail.com <C4rrera4$>
2022-07-11 22:08:39 +0100

    for loops exercises

---
+++ for loops exercise.py
@@ -0,0 +1,138 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # for Loop Exercise
+
+# Question 1.
+# Given a list of numbers, find the average using for loop
+# 
+# Number = [10, 20, 30, 40, 50, 60]
+
+# ***Hint***
+# Start with sum = 0
+
+# In[ ]:
+
+
+sum = 0
+## Type your answer here.
+
+
+
+
+
+
+# Expected Output
+# 
+# 35
+
+# 
+# 
+# 
+# 
+# Question 2. 
+# Write a program that prompts a user to enter 2 integers.
+# suppose the integers are p and q, the program then prints p rows of q hastags.
+# 
+# for instance, the program may behave as shown in the expected output
+# 
+
+# ***Hint***
+# you will need 2 for loops together
+# 
+# ***Note*** by default print() function adds a new line at the end of its output.
+# if you do not want this to happen, you have to pass in end = '' to the print() function.
+# This will remove the new line. ***Note*** the '' is made of 2 single quotes not single double quote
+# 
+# For instance
+# 
+# print ('A')
+# 
+# Print ('B')
+# 
+# gives us
+# 
+# A
+# 
+# B
+# 
+# while 
+# 
+# print ('A', end = '')
+# 
+# Print ('B', end = '')
+# 
+# gives us
+# 
+# AB
+# 
+
+# In[ ]:
+
+
+
+
+
+# Expected output
+# 
+# Lets say user entered p == 5 and q == 10
+# 
+# ##########
+# 
+# ##########
+# 
+# ##########
+# 
+# ##########
+# 
+# ##########
+
+# In[ ]:
+
+
+p = int(input('Please enter the number of rows: '))
+q = int(input('Please enter the number of Hastags per row: '))
+##Type your answer here.
+
+
+
+
+
+
+
+
+# Question 3
+# Write a for loop which print "Hello!, " plus each name in the list. i.e.: "Hello!, Ade"
+
+# In[ ]:
+
+
+lst=["Ade", "Ada", "Chidi", "Ola", "Nkechi", "Emma", "Abdul"]
+###Type your answer here.
+
+
+
+
+
+
+
+# Using a for loop and .append() method append each item with a Dr. prefix to the lst.
+
+# **HINT 1**
+# “Dr. ” + i will create a string with “Dr. ” prefix and name.
+# 
+# **HINT 2**
+# .append() method can be u
+
+# In[ ]:
+
+
+lst1=["Phil", "Oz", "Seuss", "Dre"]
+lst2=[]
+#Type your answer here.
+
+
+
+
+print(lst2)
+
