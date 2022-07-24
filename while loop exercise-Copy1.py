67ee3543bf2762153301993dd2ba6f6ef39ebd56
oawujo@gmail.com <C4rrera4$>
2022-07-24 11:30:05 +0100

    while loops exercises

---
+++ while loop exercise-Copy1.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # while loop exercise
+
+# The program will print
+# GO! GO! GO!
+# As long as the variable “x” is more that 50
+
+# In[ ]:
+
+
+# Your code here
+
+
+
+# The program that will print Good Morning, Steve as log as the variable ”name is = “Steve”
+# You will need to ask the user for their name
+# If name is not Steve print "your nname is not Steve, oh well Good Morning anyway!!
+
+# In[ ]:
+
+
+# Your code here
+
+
+
+# The program that will count from 7 to 11 by twos 3 times
+
+# In[ ]:
+
+
+# Your code here
+
+
